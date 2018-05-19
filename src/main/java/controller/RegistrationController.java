b1d517433105fb0f8859541e3c7130c706bcc722
Ilinka Poleatn <ilinka.p97@gmail.com>
2018-05-19 11:49:07 +0200

    registrationBegin

--- src/main/java/controller/RegistrationController.java
+++ src/main/java/controller/RegistrationController.java
@@ -1,4 +1,26 @@
 package controller;
 
-public class RegistrationController {
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class RegistrationController extends HttpServlet{
+	
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+	
+		String username,password;
+		
+		username=req.getParameter("username");
+		password=req.getParameter("password");
+		
+		
+		
+		
+	}
+
+	
+	
 }
--- src/main/java/database/ConnectionManager.java
+++ src/main/java/database/ConnectionManager.java
@@ -1,7 +1,6 @@
 package database;
 
 public class ConnectionManager {
-
-
+	
 
 }
